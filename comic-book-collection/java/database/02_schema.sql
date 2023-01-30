ab793088383f4acf84d44f8b24949e72a39d50c8
Quinn Peel <thomas.peel@yahoo.com>
2023-01-30 11:56:41 -0800

    Added tables for card, collection, and card_collection

--- comic-book-collection/java/database/02_schema.sql
+++ comic-book-collection/java/database/02_schema.sql
@@ -1,4 +1,8 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS card;
+DROP TABLE IF EXISTS collection;
+DROP TABLE IF EXISTS collection_card;
+
 DROP SEQUENCE IF EXISTS seq_user_id;
 
 CREATE SEQUENCE seq_user_id
@@ -14,6 +18,29 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE card (
+    card_id SERIAL PRIMARY KEY,
+    name varchar(30) NOT NULL,
+    large_image varchar(200) NOT NULL,
+    small_image varchar(200) NOT NULL
+);
+
+CREATE TABLE collection (
+    collection_id SERIAL PRIMARY KEY,
+    user_id int,
+    name varchar(50) NOT NULL,
+    description varchar(250),
+    private boolean default false,
+    CONSTRAINT FK_collection_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE collection_card (
+    collection_id int ,
+    card_id int,
+    CONSTRAINT FK_collection_card_collection FOREIGN KEY(collection_id) REFERENCES collection(collection_id),
+    CONSTRAINT FK_collection_card_card FOREIGN KEY(card_id) REFERENCES card(card_id)
 );
 
 -- Add seed data in the 03_data.sql script file
