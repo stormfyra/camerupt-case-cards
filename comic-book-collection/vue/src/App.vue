b79b346b1b58db77f79e938fc6d84d651f7dabc4
HauntedDaze <veronicalopez0350@gmail.com>
2023-01-30 16:34:58 -0800

    navBar

--- comic-book-collection/vue/src/App.vue
+++ comic-book-collection/vue/src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
       <router-link v-bind:to="{ name: 'login' }">Login</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'register' }">Register</router-link>&nbsp;
-    </div>
+    </div> -->
+    <Nav />
     <router-view />
   </div>
 </template>
+
+<script>
+import Nav from './components/Nav.vue';
+
+export default{
+  name: 'app',
+  components: {
+    Nav,
+  },
+};
+</script>
+
 
---
+++ comic-book-collection/vue/src/components/Nav.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar container">
+    <button id="home">
+    <router-link to="/" class="navbar-item">Home</router-link>
+    </button>&nbsp;
+    <button id="login" class="sign-up">
+    <router-link to="/login" class="navbar-item">Login</router-link>
+    </button>&nbsp;
+    <button id="register" class="sign-up">
+    <router-link to="/register" class="navbar-item">Register</router-link>
+    </button>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Nav',
+  };
+</script>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100vw;
+    border-bottom: solid #E45052;
+    padding-left: 30px;
+    padding-right: 30px;
+  
+  }
+
+  .sign-up{
+    display: inline-block;
+    padding: 10px 15px;
+    text-decoration: none;
+    color: white;
+  }
+</style>
