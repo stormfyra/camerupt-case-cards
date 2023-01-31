5e88f1c312a193bdb6d30a1b00b9ab444d1e4c4e
Quinn Peel <thomas.peel@yahoo.com>
2023-01-30 18:39:52 -0800

    Adding collection detail view initial commit

---
+++ comic-book-collection/vue/src/views/CollectionDetails.vue
@@ -0,0 +1,40 @@
+<template>
+    <card-grid :cards='cards'/>
+</template>
+
+<script>
+import CardGrid from "../components/Collections/CardGrid.vue" 
+import collectionWebService from '../services/CollectionService'
+
+
+export default {
+    components: {
+        CardGrid,
+    },
+    data(){
+        return {
+           ownerUsername: '',
+           title: '',
+           description: '',
+           isPrivate: '',
+           cards: []
+        }
+    },
+
+    created() {
+        collectionWebService.getCollectionDetails()
+                            .then(response => {
+                                this.ownerUsername = response.data.ownerUsername;
+                                this.title = response.data.title;
+                                this.description = response.data.description;
+                                this.isPrivate = response.data.private;
+                                this.cards = response.data.cards;
+                            })
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
