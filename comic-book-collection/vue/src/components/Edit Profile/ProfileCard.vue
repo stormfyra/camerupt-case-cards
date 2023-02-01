a9df8bc2021f08d8071f4509ec81a75db9e063ca
Storm Sternberg <storm.fyra@gmail.com>
2023-02-01 13:38:23 -0800

    made profile images accessbile.

--- comic-book-collection/vue/src/components/Edit Profile/ProfileCard.vue
+++ comic-book-collection/vue/src/components/Edit Profile/ProfileCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="profile-card">
         <h1 class="user-name">{{user.username}}</h1>
-        <img class="profile-image" src="../../../resources/backOfPokemonCard.jpg" alt="">
+        <img class="profile-image" :src='pokemonImages[pokemon]' alt="">
         <em><p class="bio"> {{user.bio}}</p></em>
         <h3>{{user.email}}</h3>
         <h3>{{user.fullName}}</h3>
@@ -12,6 +12,9 @@
 <script>
 
 export default {
+    props: [
+        'pokemon'
+    ],
     data(){
         return {
             user: {
@@ -21,12 +24,29 @@ export default {
                 email: '',
                 fullName: '',
                 shippingAddress: ''
+            },
+            pokemonImages: {
+                'Camerupt': '/img/camerupt.ea9cad37.png',
+                'Bulbasaur': '/img/bulbasaur.f0b06b8f.png',
+                'Squirtle': '/img/squirtle.6a3d8153.png',
+                'Charmander': '/img/charmander.603508ed.png',
+                'Pikachu': '/img/pikachu.2f64e2bf.png',
+                'Chikorita': '/img/chikorita.93ba3edf.png',
+                'Totodile': '/img/totodile.4c7b2c45.png',
+                'Cyndaquil': '/img/cyndaquil.647a8e91.png',
+                'Treecko': '/img/treecko.8890fffb.png',
+                'Mudkip': '/img/mudkip.926b74e1.png',
+                'Torchic': '/img/torchic.e9c21fc4.png'
             }
         }
     },
+    // computed: {
+    //     imgSrc() {
+    //         return require.context('../../../resources' + this.pokemon + '.png');
+    //     }
+    // },
     created() {
         this.user = this.$store.state.user;
-
     }
 }
 
