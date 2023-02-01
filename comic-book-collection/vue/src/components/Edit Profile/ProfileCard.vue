0a0ce27c4c13aa18745d71e87d19abfc775fe0a7
Christianne Tolentino <cat.tolentino.designs@gmail.com>
2023-02-01 12:24:05 -0800

    Tweaked user profile and added a logo to resources for testing

--- comic-book-collection/vue/src/components/Edit Profile/ProfileCard.vue
+++ comic-book-collection/vue/src/components/Edit Profile/ProfileCard.vue
@@ -1,11 +1,29 @@
 <template>
-    <div class="profile-card">
-        <h1 class="user-name">{{user.username}}</h1>
-        <img class="profile-image" src="../../../resources/backOfPokemonCard.jpg" alt="">
-        <em><p class="bio"> {{user.bio}}</p></em>
-        <h3>{{user.email}}</h3>
-        <h3>{{user.fullName}}</h3>
-        <h3>{{user.shippingAddress}}</h3>
+    <div class="profile">
+        <div class="profile-card">
+            <div class="user-header">
+                <div class="flexy">
+                    <!-- hard-coded; replace with data from database -->
+                    <p class="account-type">Premium</p>
+                    <h1 class="user-name">{{user.username}}</h1>
+                </div>
+
+                <div class="flexy">
+                    <p class="card">cards</p>
+                    <!-- hard-coded; replace with data from database -->
+                    <h1 class="card-count">347</h1>
+                    <img src="../../../resources/fire-logo.png" alt="logo" class="logo">
+                </div>
+            </div>
+            <img class="profile-image" src="../../../resources/backOfPokemonCard.jpg" alt="">
+            <h1>Bio</h1>
+            <em><p class="bio"> {{user.bio}}</p></em>
+            <h3>{{user.fullName}}</h3>
+            <h3>{{user.shippingAddress}}</h3>
+        </div>
+        <div class="page">
+
+        </div>
     </div>
 </template>
 
@@ -37,13 +55,14 @@ export default {
 .profile-card {
     display: flex;
     flex-direction: column;
-    border: solid rgb(255, 208, 0) 15px;
+    border: solid #f5e55a 23px;
     border-radius: 20px;
     width: 560px;
-    background-color: tomato;
+    background-color: #FF494C;
     margin: auto;
     margin-top: 50px;
-    padding: 3%;
+    padding: 2% 5% 5% 5%;
+
 }
 .profile-image {
     flex-grow: 4;
@@ -51,6 +70,7 @@ export default {
     width: 90%;
     margin-left: 5%;
     border-radius: 10px;
+    border: solid #D9D9D9 10px;
 }
 .bio {
     background-color: rgb(233, 76, 49);
@@ -62,5 +82,40 @@ h3 {
     border-radius: 5px;
     border: solid rgb(231, 45, 12) 3px;
 }
+
+h1 {
+    margin-bottom: 0px;
+}
+
+.logo {
+    width: 40px;
+}
+
+.user-header {
+    display: flex;
+    justify-content: space-between;
+
+}
+
+/* fix meeee; go to bottom */
+.card {
+    display: flex;
+    align-self:unset !important;
+    margin-bottom: auto;
+}
+
+.flexy {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+
+.flexy > p {
+    margin: 0 !important;
+}
+.flexy > h1 {
+    margin: 0 !important;
+}
+
 
 </style>
