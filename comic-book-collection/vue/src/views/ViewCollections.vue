e7d918bb605ed790bc4b8a1655efb646ab205681
HauntedDaze <veronicalopez0350@gmail.com>
2023-02-01 17:08:07 -0800

    made an add collection form

--- comic-book-collection/vue/src/views/ViewCollections.vue
+++ comic-book-collection/vue/src/views/ViewCollections.vue
@@ -6,6 +6,34 @@
         </div>
         <h1>Public Collections</h1>
         <collection-grid :collections="publicCollections" :ownedByMe="false"></collection-grid>
+        <h1>Add A New Collection</h1>
+        <div class="form-popup" id="form">
+            <form class="form-container">
+                <label for="title">Title</label>
+                <input 
+                    type="text"
+                    id="title"
+                    placeholder="Title"
+                    required
+                />
+                <div id="radioButton">
+                    <label for="private">Private</label>
+                    <input
+                        type="radio"
+                        id="privacy"
+                        name="privacy"
+                    />
+                    <label for="private">Public</label>
+                    <input
+                        type="radio"
+                        id="privacy"
+                        name="privacy"
+                    />
+                </div>
+                <button id="submit">Submit</button>
+                <button id="close" onClick="off()">Close</button>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -49,4 +77,23 @@ export default {
 
 <style scoped>
 
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+input{
+  width: 90%;
+  height: 20%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+}
+
+#radioButton{
+    display: flex;
+    justify-content: space-between;
+}
 </style>
