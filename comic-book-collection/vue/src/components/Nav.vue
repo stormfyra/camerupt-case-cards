d6863cd8b0be556a47f8bab64a9e9c8ee7952b22
Storm Sternberg <storm.fyra@gmail.com>
2023-02-09 00:16:40 -0800

    did navbar editing without changing it

--- comic-book-collection/vue/src/components/Nav.vue
+++ comic-book-collection/vue/src/components/Nav.vue
@@ -1,49 +1,47 @@
 <template>
-  <nav class="navbar container">
-    <a href="/" class="nav-logo"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M160 144c-35.3 0-64-28.7-64-64s28.7-64 64-64c15.7 0 30 5.6 41.2 15C212.4 12.4 232.7 0 256 0s43.6 12.4 54.8 31C322 21.6 336.3 16 352 16c35.3 0 64 28.7 64 64s-28.7 64-64 64c-14.7 0-28.3-5-39.1-13.3l-32 48C275.3 187 266 192 256 192s-19.3-5-24.9-13.3l-32-48C188.3 139 174.7 144 160 144zM144 352l48.4-24.2c10.2-5.1 21.6-7.8 33-7.8c19.6 0 38.4 7.8 52.2 21.6l32.5 32.5c6.3 6.3 14.9 9.9 23.8 9.9c11.3 0 21.8-5.6 28-15l9.7-14.6-59-66.3c-9.1-10.2-22.2-16.1-35.9-16.1H235.1c-13.7 0-26.8 5.9-35.9 16.1l-59.9 67.4L144 352zm19.4-95.8c18.2-20.5 44.3-32.2 71.8-32.2h41.8c27.4 0 53.5 11.7 71.8 32.2l150.2 169c8.5 9.5 13.2 21.9 13.2 34.7c0 28.8-23.4 52.2-52.2 52.2H52.2C23.4 512 0 488.6 0 459.8c0-12.8 4.7-25.1 13.2-34.7l150.2-169z"/></svg>
-    cameruptCaseCards</a>
-
-    <div class="nav-menu">
-      <ul class="nav-list">
-        <router-link v-if="isLoggedIn" :to="{ name: 'friends' }" class="nav-item">
-          <li class="nav-item">
-            Friends
-          </li>
-        </router-link>
-        <router-link :to="({ name: 'collections'})">
-          <li class="nav-item">
-            Card Collections
-          </li>
-        </router-link>
-        <router-link v-if="!isLoggedIn" :to="({ name: 'login'})" class="nav-item">
-          <li class="nav-item">
-            Login
-          </li>
-        </router-link>
-        <router-link v-if="!isLoggedIn" :to="{ name: 'register' }" class="nav-item">
-          <li class="nav-item">
-            Register
-          </li>
-        </router-link>
-        <router-link v-if="isLoggedIn" :to="{ name: 'logout' }" class="nav-item">
-          <li class="nav-item">
-            Logout
-          </li>
-        </router-link>
-        <div id="link-to-my-profile" @click="goToMyProfile()">
-          <li id="logged-in-user-info" v-if="isLoggedIn">
-            <div id="profile-holder-navbar">
-              <p>{{$store.state.user.username}}</p>
-            </div>
-            <div class="profile-icon-holder">
-              <profile-image :small="true" :pokemon="$store.state.user.profilePokemon" :class="'profile-icon'"></profile-image>
-            </div>    
-          </li>
-        </div>
-      </ul>
-    </div>
-
-  </nav>
+<nav>
+  <a href="/" class="nav-logo"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M160 144c-35.3 0-64-28.7-64-64s28.7-64 64-64c15.7 0 30 5.6 41.2 15C212.4 12.4 232.7 0 256 0s43.6 12.4 54.8 31C322 21.6 336.3 16 352 16c35.3 0 64 28.7 64 64s-28.7 64-64 64c-14.7 0-28.3-5-39.1-13.3l-32 48C275.3 187 266 192 256 192s-19.3-5-24.9-13.3l-32-48C188.3 139 174.7 144 160 144zM144 352l48.4-24.2c10.2-5.1 21.6-7.8 33-7.8c19.6 0 38.4 7.8 52.2 21.6l32.5 32.5c6.3 6.3 14.9 9.9 23.8 9.9c11.3 0 21.8-5.6 28-15l9.7-14.6-59-66.3c-9.1-10.2-22.2-16.1-35.9-16.1H235.1c-13.7 0-26.8 5.9-35.9 16.1l-59.9 67.4L144 352zm19.4-95.8c18.2-20.5 44.3-32.2 71.8-32.2h41.8c27.4 0 53.5 11.7 71.8 32.2l150.2 169c8.5 9.5 13.2 21.9 13.2 34.7c0 28.8-23.4 52.2-52.2 52.2H52.2C23.4 512 0 488.6 0 459.8c0-12.8 4.7-25.1 13.2-34.7l150.2-169z"/></svg>
+  cameruptCaseCards</a>
+  <div>
+    <ul class="nav-list">
+      <router-link v-if="isLoggedIn" :to="{ name: 'friends' }">
+        <li>
+          Friends
+        </li>
+      </router-link>
+      <router-link :to="({ name: 'collections'})">
+        <li>
+          Card Collections
+        </li>
+      </router-link>
+      <router-link v-if="!isLoggedIn" :to="({ name: 'login'})">
+        <li>
+          Login
+        </li>
+      </router-link>
+      <router-link v-if="!isLoggedIn" :to="{ name: 'register' }">
+        <li>
+          Register
+        </li>
+      </router-link>
+      <router-link v-if="isLoggedIn" :to="{ name: 'logout' }">
+        <li>
+          Logout
+        </li>
+      </router-link>
+      <div id="link-to-my-profile" @click="goToMyProfile()">
+        <li id="logged-in-user-info" v-if="isLoggedIn">
+          <div id="profile-holder-navbar">
+            <p>{{$store.state.user.username}}</p>
+          </div>
+          <div class="profile-icon-holder">
+            <profile-image :small="true" :pokemon="$store.state.user.profilePokemon" :class="'profile-icon'"></profile-image>
+          </div>    
+        </li>
+      </div>
+    </ul>
+  </div>
+</nav>
 </template>
 
 <script>
@@ -72,7 +70,7 @@ import ProfileImage from './Edit Profile/ProfileImage.vue';
     },
     methods: {
       goToMyProfile() {
-        this.$router.push({ name: 'profileWithId', params: {id: this.$store.state.user.id}})
+        this.$router.push({ name: 'profileWithId', params: {id: this.$store.state.user.id}});
         this.$router.go();
       }
     }
@@ -84,35 +82,34 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-bottom: solid #e93d40;
+  border-bottom: solid var(--site-red);
   padding-top: 10px;
   padding-bottom: 10px;
   font-family: "Flexo-Regular",arial,sans-serif;
-  color: #e93d40;
-
+  color: var(--site-red);
   font-size: small;
 }
+
 svg {
   width: 30px;
 }
 
-a{
+a {
   text-decoration: none;
-  color: #e93d40;
+  color: var(--site-red);
 }
 
-ul{
+ul {
   list-style: none;
 }
 
-.nav-logo{
+.nav-logo {
   font-size: 1.1rem;
   font-weight: 600;
   color: black;
   display: flex;
   align-items: center;
   gap: 10px;
-  /* font-family: 'Oswald', sans-serif; */
 }
 
 .profile-icon-holder {
@@ -123,18 +120,17 @@ height: 42px;
 .profile-icon {
   width: 100%;
   height: 100%;
-/* crops the image with the same ratio */
   object-fit: cover;
 }
 
-.nav-list{
+.nav-list {
   display: flex;
   gap: 1rem;
   flex-wrap: wrap;
 }
 
 li {
-  border: solid #e93d40 2px;
+  border: solid var(--site-red) 2px;
   border-radius: 5px;
   display: flex;
   align-items: center;
@@ -144,12 +140,12 @@ li {
   position:relative;
 }
 
-li:hover{
+li:hover {
   color:white;
   cursor: pointer;
 }
 
-li:before{
+li:before {
   transition: 0.5s all ease;
   position: absolute;
   top:0;
@@ -158,10 +154,10 @@ li:before{
   bottom: 0;
   opacity: 0;
   content: "";
-  background-color: #e93d40;
+  background-color: var(--site-red);
 }
 
-li:hover:before{
+li:hover:before {
   transition: 0.5s all ease;
   left: 0;
   right: 0;
@@ -169,18 +165,9 @@ li:hover:before{
   z-index: -1;
 }
 
-/* li:first-child {
-  border-left: none;
-} */
-
-#profile-link {
-  display: flex;
-  gap: 1rem;
-}
-
 #logged-in-user-info {
   display: flex;
-  border-left: solid #e93d40 2px;
+  border-left: solid var(--site-red) 2px;
   justify-content: center;
   gap: 5px;
 }
@@ -197,5 +184,4 @@ p {
 #link-to-my-profile:hover {
   cursor: pointer;
 }
-  
 </style>
